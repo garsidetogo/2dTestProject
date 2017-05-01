c44788d5952e5b37f07a67480c9f2a7941090637
Ben Garside <garsidetogo@gmail.com>
2017-05-01 00:35:57 -0500

    initial commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float transitionSpeed = 0.2f;
+	Camera myCam;
+
+	// Use this for initialization
+	void Start () {
+		myCam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		myCam.orthographicSize = (Screen.height / 100f) / 6f;
+		if (target) {
+			transform.position = (Vector3.Lerp (transform.position, target.position, transitionSpeed) + new Vector3(0, 0, -10));
+		}
+	}
+}
